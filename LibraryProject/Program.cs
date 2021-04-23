4ced1e42c43dc44848176cc92fb2b516030712fb
Greg Doud <gpdoud@gmail.com>
2021-04-22 21:44:20 -0400

    Committed by GitUtil

---
+++ LibraryProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Education.DB;
+
+using UtilityLibrary;
+
+namespace LibraryProject {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            var about = Utility.About();
+            Console.WriteLine($"About is {about}");
+
+            var stud1 = new Student() {
+                Id = 1, Name = "Fred", SAT = 1000, GPA = 2.5
+            };
+        }
+    }
+}
---
+++ UtilityLibrary/Utility.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace UtilityLibrary {
+
+    public class Utility {
+
+        public static string About() {
+            return "About the Utility library";
+        }
+    }
+}
